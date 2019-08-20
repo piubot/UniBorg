d4d2a5764666910cb9564c04b00966bef5e74c92
mahshookbot <52567991+mahshookbot@users.noreply.github.com>
2019-08-20 09:16:46 +0530

    Update and rename music.py to mk.py

--- stdplugins/mk.py
+++ stdplugins/mk.py
@@ -6,11 +6,11 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.music", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.mk", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("🎶🎧🎺🎵🎼🎶🎧🎼🎺🎵"))
+	deq = deque(list("MAHSHOOK"))
 	for _ in range(48):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
