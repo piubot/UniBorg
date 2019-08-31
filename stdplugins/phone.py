6a11e3b42b0161850e778ec0887107a06a7b09e4
mahshookbot <52567991+mahshookbot@users.noreply.github.com>
2019-08-31 11:06:21 +0530

    Update and rename call.py to phone.py

--- stdplugins/phone.py
+++ stdplugins/phone.py
@@ -32,7 +32,7 @@ async def _(event):
 
     input_str = event.pattern_match.group(1)
 
-    if input_str == "/cull":
+    if input_str == "phone":
 
         await event.edit(input_str)
 
@@ -41,21 +41,22 @@ async def _(event):
             "`Connecting To Telegram Headquarters...`",
             "`Call Connected.`",
             "`Telegram: Hello This is Telegram HQ. Who is this?`",
-            "`Me: Yo this is` @r4v4n4 ,`Please Connect me to Pavel Durov Shukla`",
+            "`Me: Yo this is` @mahshook_bot ,`Please Connect me to Pavel Durov Shukla`",
             "`User Authorised.`",
             "`Calling Pavel Durov Shukla (@durov) At +916969696969`",
             "`Private  Call Connected...`",
             "`Me: Hello Sir, Please Ban This Telegram Account.`",    
             "`Durov: May I Know Who Is This?`",
-            "`Me: Yo Brah, I Am` @r4v4n4",
+            "`Me: Yo Brah, I Am` @mahshook_bot",
             "`Durov: OMG!!! I Am FAN Of You Sir...\nI'll Make Sure That Guy Account Will Get Blocked Within 24Hrs.`",
             "`Me: Thanks, See You Later Brah.`",
             "`Durov: Please Don't Thank Sur, Telegram Is Your's. Just Gimme A Call When You Become Free.`",
             "`Me: Is There Any Issue/Emergency???`",
-            "`Durov: Yes Sur, There Is A Bug In Telegram v5.8.0.\nI Am Not Able To Fix It. If Possible, Please Help Fix The Bug.`",
+            "`Durov: Yes Sur, There Is A Bug In new version of Telegram\nI Am Not Able To Fix It. If Possible, Please Help Fix The Bug.`",
             "`Me: Send Me The App On My Telegram Account, I Will Fix The Bug & Send You.`",
             "`Durov: Sure Sur \nTC Bye Bye :)`",
             "`Private Call Disconnected.`"
+            "`This account mentioned above will be banned soon by telegram.\ncontact @mahshook_bot to know further details regarding this.`"
         ]
 
         for i in animation_ttl:
