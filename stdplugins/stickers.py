e8d489710af66cc1b766c8db1683c5f23f8b2497
mahshookbot <52567991+mahshookbot@users.noreply.github.com>
2019-09-27 18:22:35 +0530

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -44,7 +44,7 @@ async def _(event):
 
     me = borg.me
     userid = event.from_id
-    packname = f"{userid}'s @UniBorg Pack"
+    packname = f"RoBoy_AnS_pack1"
     packshortname = f"Uni_Borg_{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
